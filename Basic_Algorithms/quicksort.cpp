3f8b816bc26d53937dfb0242e380c8c62f77e0cf
georgistoilov8 <gogo.stoilov@abv.bg>
2018-10-26 23:56:06 +0300

    Add example to quicksort

--- Basic_Algorithms/quicksort.cpp
+++ Basic_Algorithms/quicksort.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+void print(int* array, int size) {
+	for (int i = 0; i < size; i++) {
+		std::cout << array[i] << ' ';
+	}
+	std::cout << std::endl;
+}
+
 void quicksort(int* arr, int size) {
 	if (size > 1) {
 		int middle_element = arr[std::rand() % size];
@@ -27,6 +34,10 @@ void quicksort(int* arr, int size) {
 }
 
 int main(){
-	
-  return 0;
+	// Example:
+	int test_arr[] = { 1,2,3,4,20,15,13,12,116, 19, 59, 39, 18, 5,6,7,8,50 };
+	int size = sizeof(test_arr) / sizeof(test_arr[0]);
+	quicksort(test_arr, size);
+	print(test_arr, size);
+  	return 0;
 }
