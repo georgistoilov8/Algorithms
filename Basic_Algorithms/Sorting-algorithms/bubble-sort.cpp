2bfee8c257549514f1bd6f3b86d9f696c231f05c
Georgi <gogo.stoilov@abv.bg>
2018-12-19 15:59:12 +0200

    Template bubble sort

---
+++ Basic_Algorithms/Sorting-algorithms/bubble-sort.cpp
@@ -0,0 +1,38 @@
+/*
+	Author: Georgi Stoilov
+*/
+
+#include <iostream>
+
+/* Възходящ ред */
+template <typename T>
+void buble_sort(T* arr, unsigned arr_size) {
+	bool is_sorted = false;
+	while (!is_sorted) {
+		is_sorted = true;
+		for (unsigned i = 0; i < arr_size-1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				std::swap(arr[i], arr[i + 1]);
+				is_sorted = false;
+			}
+		}
+	}
+		
+}
+
+int main() {
+	std::cout << "Input array size: ";
+	int n;
+	std::cin >> n;
+	int* arr = new int[n];
+	std::cout << "Input array: ";
+	for (int i = 0; i < n; i++) {
+		std::cin >> arr[i];
+	}
+	buble_sort<int>(arr, n);
+	for (int i = 0; i < n; i++) {
+		std::cout << arr[i] << ' ';
+	}
+	system("pause");
+	return 0;
+}
