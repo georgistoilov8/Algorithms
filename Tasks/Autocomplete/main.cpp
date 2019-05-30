94bb2e235e8be92090766f9276a2300f92a43ba0
Georgi Stoilov <gogo.stoilov@abv.bg>
2019-05-30 18:58:01 +0300

    Renaming variables

--- Tasks/Autocomplete/main.cpp
+++ Tasks/Autocomplete/main.cpp
@@ -3,12 +3,12 @@
 
 int main(int argc, char* argv[]) {
 	
-	int k;
+	int suggestions_count;
 	std::cout << "Please give number of suggestions you want to see: ";
-	std::cin >> k;
+	std::cin >> suggestions_count;
 	std::cin.ignore();
 	System s;
-	s.start(argv[1], k);
+	s.start(argv[1], suggestions_count);
 	
 	return 0;
 }
